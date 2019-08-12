31f159d3e3951175393a0774b134ba28472cc1aa
mdouglas630 <pes.m.douglas.630@gmail.com>
2019-08-12 14:42:23 -0300

    primeiro comentario

---
+++ Untitled-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void){
+    system("clear");
+    int opc, opc2;
+    printf("Bem vindo ao Digite.me\n");
+    printf("1 - Iniciar Novo Jogo\n");
+    printf("2 - Continuar Jogo\n");
+    printf("3 - Historico de pontos\n");
+    printf("4 - Adicionar Novas Palavras\n");
+    printf("5 - Editar Novas Palavras\n");
+    printf("6 - Excluir Palavras\n");
+    printf("7 - Sobre o Programa");
+    
+}
