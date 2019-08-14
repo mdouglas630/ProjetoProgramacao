7d509513903ff292a79649f44363941a755a92d6
mdouglas630 <pes.m.douglas.630@gmail.com>
2019-08-14 15:53:16 -0300

    melhorias

--- Untitled-1.c
+++ Untitled-1.c
@@ -3,19 +3,33 @@
 
 void main(void){
     system("clear");
-    int opc, opc2;
+    int opc;
     printf("Bem vindo ao Digite.me\n");
     printf("1 - Iniciar Novo Jogo\n");
     printf("2 - Continuar Jogo\n");
     printf("3 - Historico de pontos\n");
-    printf("4 - Adicionar Novas Palavras\n");
-    printf("5 - Editar Novas Palavras\n");
-    printf("6 - Excluir Palavras\n");
-    printf("7 - Sobre o Programa");
+    printf("4 - Sobre o Programa");
 }
 
 void sobre(void){
     printf("Autor do Programa: Maicon Douglas\n");
     printf("Email do Autor: pes.m.douglas.630@gmail.com\n");
     printf("Facebook do Autor: facebook.com/maicondouglas5\n");
+}
+
+void login(void){
+    char login[12], senha[10];
+    printf("Digite o seu login: ");
+    scanf(" %c", &login);
+    printf("Digite a sua senha: ");
+    scanf(" %c", &senha);
+}
+
+void newplayer(void){
+    char login[12], senha[10];
+    printf("digite o seu novo login(até 12 caracteres): ");
+    scanf(" %c", &login);
+    printf("digite uma senha (até 10 caracteres): ");
+    scanf(" %c", &senha);
+
 }
